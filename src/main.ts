ab60d3b14a264831e81a0877349552526ec29d10
rocky9091 <rocky46311@gmail.com>
2026-03-09 16:41:45 +0900

    fix error

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:3000', 'https://match-rate-frontend.vercel.app'] });
+  app.enableCors({ origin: ['http://localhost:3000', 'https://match-rate.vercel.app'] });
   await app.listen(3001);
 }
 bootstrap();
