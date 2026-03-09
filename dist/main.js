69354d4fb9fb0ef9d4d9d5331a05ccf4f9603f68
rocky9091 <rocky46311@gmail.com>
2026-03-09 16:39:33 +0900

    fix cross origin_error

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors({ origin: 'http://localhost:3000' });
+    app.enableCors({ origin: ['http://localhost:3000', 'https://match-rate-frontend.vercel.app'] });
     await app.listen(3001);
 }
 bootstrap();
